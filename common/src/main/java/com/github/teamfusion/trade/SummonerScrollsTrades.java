21ad4e5aecb8e5d524014d97c4e7b2a3bed00517
StevenPlayzz <playzzsteven@gmail.com>
2022-08-22 16:47:24 -0400

    change from weaponsmith to cleric traders

--- common/src/main/java/com/github/teamfusion/trade/SummonerScrollsTrades.java
+++ common/src/main/java/com/github/teamfusion/trade/SummonerScrollsTrades.java
@@ -8,28 +8,16 @@ import net.minecraft.world.entity.npc.VillagerTrades;
 public class SummonerScrollsTrades {
     public static void init() {
         SummonerScrollsItems.ZOMBIE_SCROLL.listen((item) -> TradeRegistry.registerVillagerTrade(
-                VillagerProfession.TOOLSMITH, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
-        ));
-        SummonerScrollsItems.ZOMBIE_SCROLL.listen((item) -> TradeRegistry.registerVillagerTrade(
-                VillagerProfession.WEAPONSMITH, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
+                VillagerProfession.CLERIC, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
         ));
         SummonerScrollsItems.SKELETON_SCROLL.listen((item) -> TradeRegistry.registerVillagerTrade(
-                VillagerProfession.TOOLSMITH, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
-        ));
-        SummonerScrollsItems.SKELETON_SCROLL.listen((item) -> TradeRegistry.registerVillagerTrade(
-                VillagerProfession.WEAPONSMITH, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
+                VillagerProfession.CLERIC, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
         ));
         SummonerScrollsItems.SPIDER_SCROLL.listen((item) -> TradeRegistry.registerVillagerTrade(
-                VillagerProfession.TOOLSMITH, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
-        ));
-        SummonerScrollsItems.SPIDER_SCROLL.listen((item) -> TradeRegistry.registerVillagerTrade(
-                VillagerProfession.WEAPONSMITH, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
+                VillagerProfession.CLERIC, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
         ));
         SummonerScrollsItems.ENDERMAN_SCROLL.listen((item) -> TradeRegistry.registerVillagerTrade(
-                VillagerProfession.TOOLSMITH, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
-        ));
-        SummonerScrollsItems.ENDERMAN_SCROLL.listen((item) -> TradeRegistry.registerVillagerTrade(
-                VillagerProfession.WEAPONSMITH, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
+                VillagerProfession.CLERIC, 1, new VillagerTrades.EmeraldForItems(SummonerScrollsItems.ZOMBIE_SCROLL.get(), 1, 5, 2)
         ));
     }
 }
