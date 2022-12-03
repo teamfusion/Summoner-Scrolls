92e5828aca4b201fe8569b815d4f00f422f3f449
StevenPlayzz <playzzsteven@gmail.com>
2022-12-02 19:19:25 -0500

    Loot tables - summons now drop scrolls (1% chance)

--- fabric/src/datagen/java/com/github/teamfusion/summonerscrolls/datagen/SummonerScrollsDataGenerator.java
+++ fabric/src/datagen/java/com/github/teamfusion/summonerscrolls/datagen/SummonerScrollsDataGenerator.java
@@ -2,6 +2,7 @@ package com.github.teamfusion.summonerscrolls.datagen;
 
 import com.github.teamfusion.summonerscrolls.datagen.client.LanguageGenerator;
 import com.github.teamfusion.summonerscrolls.datagen.client.ModelGenerator;
+import com.github.teamfusion.summonerscrolls.datagen.common.loot.EntityLootGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -10,6 +11,7 @@ public final class SummonerScrollsDataGenerator implements DataGeneratorEntrypoi
     public void onInitializeDataGenerator(FabricDataGenerator gen) {
         gen.addProvider(ModelGenerator::new);
         gen.addProvider(LanguageGenerator::new);
+        gen.addProvider(EntityLootGenerator::new);
     }
     //todo: particle and sounds generator
 }
