29f1af7abaa272edcbf56c274e73f6576aaa51ed
Steve <playzzsteven@gmail.com>
2022-12-26 18:30:05 -0500

    Fix missing ItemProperties

--- common/src/main/java/com/github/teamfusion/summonerscrolls/client/SSClient.java
+++ common/src/main/java/com/github/teamfusion/summonerscrolls/client/SSClient.java
@@ -10,9 +10,12 @@ import com.github.teamfusion.summonerscrolls.client.render.entity.SpiderSummonRe
 import com.github.teamfusion.summonerscrolls.client.render.entity.StraySummonRenderer;
 import com.github.teamfusion.summonerscrolls.client.render.entity.ZombieSummonRenderer;
 import com.github.teamfusion.summonerscrolls.common.registry.SSEntityTypes;
+import com.github.teamfusion.summonerscrolls.common.registry.SSItems;
 import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
+import dev.architectury.registry.item.ItemPropertiesRegistry;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.resources.ResourceLocation;
 
 
 @Environment(EnvType.CLIENT)
@@ -31,5 +34,15 @@ public class SSClient extends SummonerScrolls {
 
         EntityRendererRegistry.register(SSEntityTypes.CREEPER_SUMMON, CreeperSummonRenderer::new);
         EntityRendererRegistry.register(SSEntityTypes.CHARGED_CREEPER_SUMMON, CreeperSummonRenderer::new);
+
+        ItemPropertiesRegistry.register(SSItems.SUMMON_BOW.get(), new ResourceLocation("pull"), (itemStack, clientLevel, livingEntity, i) -> {
+            if (livingEntity == null) {
+                return 0.0F;
+            } else {
+                return livingEntity.getUseItem() != itemStack ? 0.0F : (float)(itemStack.getUseDuration() - livingEntity.getUseItemRemainingTicks()) / 20.0F;
+            }
+        });
+
+        ItemPropertiesRegistry.register(SSItems.SUMMON_BOW.get(), new ResourceLocation("pulling"), (itemStack, clientLevel, livingEntity, i) -> livingEntity != null && livingEntity.isUsingItem() && livingEntity.getUseItem() == itemStack ? 1.0F : 0.0F);
     }
 }
